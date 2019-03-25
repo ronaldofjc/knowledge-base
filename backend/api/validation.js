4f9453aa007ae0ea7fcf3a75ebe4bae8391893c2
Ronaldo <ronaldofjc@hotmail.com>
2019-03-25 11:23:41 -0300

    Tables and Migrations

--- backend/api/validation.js
+++ backend/api/validation.js
@@ -2,13 +2,13 @@ module.exports = () => {
   function existsOrError(value, msg) {
     if (!value) throw msg;
     if (Array.isArray(value) && value.length === 0) throw msg;
-    if (typeof value === 'string' && !value.trim()) throw msg;
+    if (typeof value === "string" && !value.trim()) throw msg;
   }
 
   function notExistsOrError(value, msg) {
     try {
       existsOrError(value, msg);
-    } catch (error) {
+    } catch (msg) {
       return;
     }
     throw msg;
