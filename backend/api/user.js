5a3654125cdae7456cc29cc3a69671ac9d3d1f16
Ronaldo <ronaldofjc@hotmail.com>
2019-04-13 15:45:02 -0300

    Validation Create User Admin

--- backend/api/user.js
+++ backend/api/user.js
@@ -10,7 +10,10 @@ module.exports = app => {
 
   const save = async (req, res) => {
     const user = { ...req.body };
+
     if (req.params.id) user.id = req.params.id;
+    if (!req.originalUrl.startsWith("/users")) user.admin = false;
+    if (!req.user || !req.user.admin) user.admin = false;
 
     try {
       existsOrError(user.name, "Nome não informado!");
