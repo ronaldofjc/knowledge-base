c113ae57a1a538fc0fc4873224e5c40ab6bde5a7
Ronaldo <ronaldofjc@hotmail.com>
2019-04-17 20:58:45 -0300

    Admin Pages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,8 @@ import router from "./config/router";
 Vue.config.productionTip = false;
 
 // TEMPORARIO
-require("axios").defaults.headers.common["Authorization"] = "bearer ";
+require("axios").defaults.headers.common["Authorization"] =
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJvbmFsZG8gRmVpdG9zYSIsImVtYWlsIjoicm9uYWxkb2ZqY0Bob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NTU1NDI0NzIsImV4cCI6MTU1NTgwMTY3Mn0.yfgWjOW9DkOWScXJJ4KcKR8wDNeQplbX1N8ph2b-Pw8";
 
 new Vue({
   router,
