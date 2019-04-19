b345ec550f243fd7fcff74c31a54f7b50ab478fa
Ronaldo <ronaldofjc@hotmail.com>
2019-04-19 11:31:19 -0300

    Vue Toasted

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,1 +1,15 @@
-export const baseApiUrl = "http://localhost:3000";
+import Vue from "vue";
+
+const baseApiUrl = "http://localhost:3000";
+
+function showError(e) {
+  if (e && e.response && e.response.data) {
+    Vue.toasted.global.defaultError({ msg: e.response.data });
+  } else if (typeof e === "string") {
+    Vue.toasted.global.defaultError({ msg: e });
+  } else {
+    Vue.toasted.global.defaultError();
+  }
+}
+
+export default { baseApiUrl, showError };
