f08ef71a214182a6b05a38062c0d0628bdc17a6b
Ronaldo <ronaldofjc@hotmail.com>
2019-04-19 15:42:02 -0300

    User Registration #3

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -10,6 +10,7 @@
               type="text"
               v-model="user.name"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o Nome do Usuário..."
             />
           </b-form-group>
@@ -21,13 +22,19 @@
               type="text"
               v-model="user.email"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o E-mail do Usuário..."
             />
           </b-form-group>
         </b-col>
       </b-row>
-      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3">Administrador?</b-form-checkbox>
-      <b-row>
+      <b-form-checkbox
+        id="user-admin"
+        v-model="user.admin"
+        v-show="mode === 'save'"
+        class="mt-3 mb-3"
+      >Administrador?</b-form-checkbox>
+      <b-row v-show="mode === 'save'">
         <b-col md="6" sm="12">
           <b-form-group label="Senha:" label-for="user-password">
             <b-form-input
@@ -51,12 +58,25 @@
           </b-form-group>
         </b-col>
       </b-row>
-      <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
-      <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
-      <b-button class="ml-2" @click="reset">Cancelar</b-button>
+      <b-row>
+        <b-col xs="12">
+          <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+          <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+          <b-button class="ml-2" @click="reset">Cancelar</b-button>
+        </b-col>
+      </b-row>
     </b-form>
     <hr>
-    <b-table hover striped :items="users" :fields="fields"></b-table>
+    <b-table hover striped :items="users" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -91,6 +111,10 @@ export default {
       axios.get(url).then(res => {
         this.users = res.data;
       });
+    },
+    loadUser(user, mode = "save") {
+      this.mode = mode;
+      this.user = { ...user };
     },
     reset() {
       this.mode = "save";
