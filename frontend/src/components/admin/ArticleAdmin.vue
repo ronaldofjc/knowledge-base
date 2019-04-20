e30c263a12f43dec98356458e22213c753cc0843
Ronaldo <ronaldofjc@hotmail.com>
2019-04-20 14:13:17 -0300

    Articles - Pagination

--- frontend/src/components/admin/ArticleAdmin.vue
+++ frontend/src/components/admin/ArticleAdmin.vue
@@ -63,6 +63,7 @@
         </b-button>
       </template>
     </b-table>
+    <b-pagination size="md" v-model="page" :total-rows="count" :per-page="limit"/>
   </div>
 </template>
 
@@ -94,7 +95,7 @@ export default {
   },
   methods: {
     loadArticles() {
-      const url = `${baseApiUrl}/articles`;
+      const url = `${baseApiUrl}/articles?page=${this.page}`;
       axios.get(url).then(res => {
         this.articles = res.data.listArticles;
         this.count = res.data.count;
@@ -148,6 +149,11 @@ export default {
           return { value: user.id, text: `${user.name} - ${user.email}` };
         });
       });
+    }
+  },
+  watch: {
+    page() {
+      this.loadArticles();
     }
   },
   mounted() {
