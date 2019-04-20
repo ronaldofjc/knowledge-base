c03391789c209b92e22ee193b199406cf3d45fce
Ronaldo <ronaldofjc@hotmail.com>
2019-04-20 14:53:52 -0300

    Articles by Category

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 
 import Home from "@/components/home/Home";
 import AdminPages from "@/components/admin/AdminPages";
+import ArticlesByCategory from "@/components/article/ArticlesByCategory";
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,11 @@ const routes = [
     name: "adminPages",
     path: "/admin",
     component: AdminPages
+  },
+  {
+    name: "articlesByCategory",
+    path: "/categories/:id/articles",
+    component: ArticlesByCategory
   }
 ];
 
