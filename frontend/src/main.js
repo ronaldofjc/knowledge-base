fe9a4385c6fc6f38881586a3540b581aa1ecacaf
Ronaldo <ronaldofjc@hotmail.com>
2019-04-23 22:04:57 -0300

    Article By Id

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false;
 
 // TEMPORARIO
 require("axios").defaults.headers.common["Authorization"] =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJvbmFsZG8gRmVpdG9zYSIsImVtYWlsIjoicm9uYWxkb2ZqY0Bob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NTU2ODUwMjksImV4cCI6MTU1NTk0NDIyOX0.5-dOrk50zN1DWokzza8GphId2npwTtq1pq6l6l5VEao";
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJvbmFsZG8gRmVpdG9zYSIsImVtYWlsIjoicm9uYWxkb2ZqY0Bob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NTYwNjUzMjQsImV4cCI6MTU1NjMyNDUyNH0.1L9h05Lsjsiwt-8DG02wGHLSDu4oEAyddZydNhY2PGg";
 
 new Vue({
   router,
