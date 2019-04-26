1839694a0421fcabffd7771cdec1df6ab0d56e29
Ronaldo <ronaldofjc@hotmail.com>
2019-04-25 21:41:53 -0300

    Application Menu #1

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -1,13 +1,46 @@
 <template>
-  <aside class="menu" v-show="isMenuVisible"></aside>
+  <aside class="menu" v-show="isMenuVisible">
+    <div class="menu-filter">
+      <i class="fa fa-search fa-lg"></i>
+      <input
+        type="text"
+        placeholder="Digite para filtrar..."
+        v-model="treeFilter"
+        class="filter-field"
+      >
+    </div>
+    <Tree :data="treeData" :options="treeOptions" :filter="treeFilter" ref="tree"/>
+  </aside>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import Tree from "liquor-tree";
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 
 export default {
   name: "Menu",
-  computed: mapState(["isMenuVisible"])
+  components: { Tree },
+  computed: mapState(["isMenuVisible"]),
+  data: function() {
+    return {
+      treeData: this.getTreeData(),
+      treeFilter: "",
+      treeOptions: {
+        propertyNames: { text: "name" },
+        filter: {
+          emptyText: "Categoria não encontrada!"
+        }
+      }
+    };
+  },
+  methods: {
+    getTreeData() {
+      const url = `${baseApiUrl}/categories/tree`;
+      return axios.get(url).then(res => res.data);
+    }
+  }
 };
 </script>
 
@@ -19,5 +52,50 @@ export default {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+}
+
+.menu a,
+.menu a:hover {
+  color: #fff;
+  text-decoration: none;
+}
+
+.menu .tree-node.selected > .tree-content,
+.menu .tree-node .tree-content:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.tree-arrow.has-child {
+  filter: brightness(2);
+}
+
+.menu .menu-filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #aaa;
+}
+
+.menu .menu-filter i {
+  color: #aaa;
+  margin-right: 10px;
+}
+
+.menu input {
+  color: #ccc;
+  font-size: 1.1rem;
+  border: 0;
+  outline: 0;
+  width: 100%;
+  background: transparent;
+}
+
+.tree-filter-empty {
+  color: #ccc;
+  font-size: 1.1rem;
+  margin-left: 20px;
 }
 </style>
