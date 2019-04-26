745c866c7250d6b814188ab8f98035679c1704e8
Ronaldo <ronaldofjc@hotmail.com>
2019-04-26 20:23:59 -0300

    Application Menu #2

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -39,7 +39,16 @@ export default {
     getTreeData() {
       const url = `${baseApiUrl}/categories/tree`;
       return axios.get(url).then(res => res.data);
+    },
+    onNodeSelect(node) {
+      this.$router.push({
+        name: "articlesByCategory",
+        params: { id: node.id }
+      });
     }
+  },
+  mounted() {
+    this.$refs.tree.$on("node:selected", this.onNodeSelect);
   }
 };
 </script>
